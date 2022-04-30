821f9e143a38521a037d4ae04d4ecee03a0bfa69
Sergei Popov <hvunt32@gmail.com>
2022-04-30 22:00:59 +0300

    excluding .cmake from git tracking

--- main.c
+++ main.c
@@ -2,26 +2,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 #include "lpa.h"
 
 int main(int argc, char const *argv[]){
-    //this is used for obstacles making
+    //this is used for making obstacles
     srand(time(NULL));
 
     int X = 0, Y = 0;
-    PQ_list_t *queue = NULL;
     list_t *path = NULL;
-    printf("LPA init code: %i\n", lpa_init(200, 200));
+
+    printf("LPA init code: %i\n", lpa_init(20, 20));
     while (1){
         printf("Enter goal X coordinate\n");
         scanf("%d", &X);
         printf("Enter goal Y coordinate\n");
         scanf("%d", &Y);
-        printf("Is path was found: %d\n", lpa_compute_path(queue, path, X, Y));
         
-        // list_free(&path);
-    }
+        int is_path_found = lpa_compute_path(X, Y);
+        printf("Is path was found: %d\n", is_path_found);
 
+        if (is_path_found > 0){
+            path = lpa_get_path();
+            lpa_show_map(X, Y);
+            while (path != NULL)
+            {
+                node_t node = list_pop(&path);
+                printf("X: %d; Y: %d\n", node.x, node.y);
+            }
+            // list_free(path);
+        }
+    }
+    lpa_deinit();
     return 1;
 }
